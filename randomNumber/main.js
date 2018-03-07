7a3c8dfa8bbb1e432b18c8560110b7ee246c0811
Rafael Chavez <rafa.chavez.torres@gmail.com>
2018-03-06 22:47:29 -0600

    Clean files

--- randomNumber/main.js
+++ randomNumber/main.js
@@ -3,5 +3,23 @@ function flip() {
 }
 
 function randomNumber(n) {
-  console.log(flip());
-}
+  var date = new Date();
+  var number = ""  + date.getMilliseconds() + date.getSeconds();
+  var result = 0;
+
+  if (n.toString().length < 4) {
+  	number = number.substring(0, n.toString().length);
+  }
+
+  if (flip()) {
+  	result = Math.abs(n - parseInt(number));
+  } else {
+  	result = Math.abs(parseFloat(n / parseInt(number)).toFixed(0));
+  }
+
+  while(result > n){
+  	result = parseFloat(result / 2).toFixed(0);
+  }
+
+  console.log(result);
+			}
